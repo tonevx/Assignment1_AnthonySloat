fde428549903d4d0459ee7642da8b7e1d285cf6a
tonevx <anthonysloat02@gmail.com>
2026-09-04 14:39:04 -0400

    Collisions Update
    Added collisions to the ship and the asteroid. With this, I did want to do a bit more, so I looked up a bit of basic collision syntax to help myself out, and learned a little about making tags to do so.

---
+++ Assets/Scripts/ShipCollision.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ShipCollision : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Asteroid"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
